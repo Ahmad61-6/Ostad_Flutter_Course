714a516d22f712fa13bdb8c7681e278d1ee3a5cb
Ahmad61_6 <ahmadmizan210@gmail.com>
2023-09-26 22:50:07 +0600

    Assignment_mod_7.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main()
-{
-      runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,36 +16,36 @@ class MyApp extends StatelessWidget {
 }
 
 class ProductList extends StatefulWidget {
-  const ProductList({super.key});
-
   @override
   State<ProductList> createState() => _ProductListState();
 }
 
 class _ProductListState extends State<ProductList> {
-  List<Products> products = [
-  Products("Product 1", 20.00),
-  Products("Product 2", 20.00),
-  Products("Product 3", 20.00),
-  Products("Product 4", 20.00),
-  Products("Product 5", 20.00),
-  Products("Product 6", 20.00),
-  Products("Product 7", 20.00),
-  Products("Product 8", 20.00),
-  Products("Product 9", 20.00),
-  Products("Product 10", 20.00),
-  Products("Product 11", 20.00),
-  Products("Product 12", 20.00),
-  Products("Product 13", 20.00),
-  Products("Product 14", 20.00),
-  Products("Product 15", 20.00),
-  Products("Product 16", 20.00),
-  Products("Product 17", 20.00),
-  Products("Product 18", 20.00),
-  Products("Product 19", 20.00),
-  Products("Product 20", 20.00),
-  ];
-  
+  _CongratulationAlertDialog(Products product) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Congratulations !"),
+            content: Text(
+                "You have brought ${product.quantity} ${product.name}!!"),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK")),
+            ],
+          );
+        });
+  }
+
+  List<Products> products = List.generate(
+      20, (index) => Products('Product ${index + 1}', 20.00));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,13 +53,16 @@ class _ProductListState extends State<ProductList> {
         title: Text('Product List'),
         centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () { 
-        Navigator.push(context, MaterialPageRoute(builder: (context)=> CartPage()));
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => CartPage(products: products)));
+        },
         child: Icon(Icons.shopping_cart),
       ),
-      body: ListView.separated(itemCount: products.length,
-        itemBuilder: (context,index){
+      body: ListView.separated(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
           Products product = products[index];
           return Padding(
             padding: const EdgeInsets.all(8.0),
@@ -72,56 +72,80 @@ class _ProductListState extends State<ProductList> {
               trailing: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Counter: ${product.quantity}',
-                  style: TextStyle(
-                    fontSize: 15,
+                  Text(
+                    'Counter: ${product.quantity}',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
                   ),
-                  ),
-                  ElevatedButton(onPressed: (){
-
-                  }, child: Text('Buy now'))
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          product.quantity += 1;
+                          if (product.quantity >= 5) {
+                            _CongratulationAlertDialog(product);
+                          }
+                        });
+                      },
+                      child: Text('Buy now'))
                 ],
               ),
             ),
           );
         },
         separatorBuilder: (context, index) {
-          return Divider(
-          );
+          return Divider();
         },
-        ),
-    );
-  }
-}
-
-class CartPage extends StatefulWidget {
-  const CartPage({super.key});
-
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cart'),
-        centerTitle: true,
       ),
     );
   }
 }
 
+class CartPage extends StatelessWidget {
+  final List<Products> products;
 
+  CartPage({required this.products});
 
+  @override
+  Widget build(BuildContext context) {
+    int totalCount = products.fold(0, (sum, item) => sum + item.quantity);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Total products bought: $totalCount',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                if (products[index].quantity == 0) return SizedBox.shrink();
+
+                return ListTile(
+                  title: Text(products[index].name),
+                  trailing: Text('x${products[index].quantity}'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class Products {
   String name;
   double price;
-  int totalProductCount = 0;
   int quantity = 0;
 
   Products(this.name, this.price);
 }
-
