9bcacfc009811b357368afd342fd8e40b9ca7041
Ahmad61_6 <ahmadmizan210@gmail.com>
2023-09-30 21:13:19 +0600

    Assignment_mod_8.

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp> {
     return ResponsiveApp(
       builder: (context) {
         return MaterialApp(
-          title: 'Live Test 08',
+          title: 'Assignment 08',
           home: HomePage(),
         );
       },
@@ -36,34 +36,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var myItems = [
     {
-      "img": "https://via.placeholder.com/150",
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
     },
     {
-      "img": "https://via.placeholder.com/150",
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
     },
     {
-      "img": "https://via.placeholder.com/150",
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
     },
     {
-      "img": "https://via.placeholder.com/150",
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
     },
     {
-      "img": "https://via.placeholder.com/150",
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
     },
     {
-      "img": "https://via.placeholder.com/150",
-    },
-    {
-      "img": "https://via.placeholder.com/150",
-    },
-    {
-      "img": "https://via.placeholder.com/150",
-    },
-    {
-      "img": "https://via.placeholder.com/150",
-    },
-    {
-      "img": "https://via.placeholder.com/150",
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
     },
   ];
 
@@ -71,21 +65,48 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text("News Feed"),
+        title:  Text("Profile"),
       ),
       body: OrientationBuilder(
         builder: (context, orientation) {
           final isLandscape = orientation == Orientation.landscape;
-          final crossAxisCount = isLandscape ? 2 : 1;
+          final crossAxisCount = isLandscape ? 3 : 3;
 
           return isLandscape
               ? Row(
             children: [
               Expanded(
+                flex: 1,
+                child: ClipOval(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                        'https://media.gettyimages.com/id/1450131991/photo/lionel-messi-of-argentina-kisses-the-fifa-world-cup-qatar-2022-winners-trophy-after-the-fifa.jpg?s=612x612&w=0&k=20&c=wliZkz5XPFqvJqs5rqt9s2sNzsfRDxF-fHHFDyOQ-LE=',
+                        fit: BoxFit.cover),
+                  ),
+                ),
+              ),
+              Expanded(
                 flex: 2,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "Messi (G.O.A.T)",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                      child: Text(
+                          'Lionel Messi has conquered his final peak. Lionel Messi has shaken hands with paradise. The little boy from Rosario, Santa Fe, has just pitched up in heaven.'),
+                    ),
                     Expanded(
                       child: GridView.builder(
                         gridDelegate:
@@ -116,6 +137,30 @@ class _HomePageState extends State<HomePage> {
           )
               : Column(
             children: [
+              ClipOval(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  child: Image.network(
+                      'https://media.gettyimages.com/id/1450131991/photo/lionel-messi-of-argentina-kisses-the-fifa-world-cup-qatar-2022-winners-trophy-after-the-fifa.jpg?s=612x612&w=0&k=20&c=wliZkz5XPFqvJqs5rqt9s2sNzsfRDxF-fHHFDyOQ-LE=',
+                      fit: BoxFit.cover),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "Messi (The G.O.A.T)",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                child: Text(
+                    'Lionel Messi has conquered his final peak. Lionel Messi has shaken hands with paradise. The little boy from Rosario, Santa Fe, has just pitched up in heaven.'),
+              ),
               Expanded(
                 child: GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -132,6 +177,7 @@ class _HomePageState extends State<HomePage> {
                         height: isLandscape ? 100 : 150,
                         child: Image.network(
                           myItems[index]['img']!,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     );
