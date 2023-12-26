f7aa58a97a441c98b23567a2e60e953ac3df7307
Ahmad61_6 <ahmadmizan210@gmail.com>
2023-12-26 16:03:50 +0600

    Module_8 ----Responsive Design---- -Responsive layout Builder -device preview for different size device -Sizer package - height, width

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,205 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ostadapp/pages/home_page.dart';
-import '';
 
 void main()
 {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      title: 'Assignment 8',
       home: HomePage(),
     );
   }
-
 }
-class HomePage extends StatelessWidget{
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myItems = [
+    {
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
+    },
+    {
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
+    },
+    {
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
+    },
+    {
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
+    },
+    {
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
+    },
+    {
+      "img":
+      "https://media.gettyimages.com/id/1451936359/photo/lionel-messi-of-argentina-lifts-the-fifa-world-cup-qatar-2022-winners-trophy-during-the-fifa.jpg?s=612x612&w=0&k=20&c=B0UM5Hko74IGwcC2qLq5dcsZhwfgGOVaWVJyfRLAOLA=",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: Colors.green,
-      toolbarHeight: 100,
-      elevation: 70.0,
-      centerTitle: true,
-        leading: Icon(
-          Icons.add_business_rounded, color: Colors.white,
-        ),
-        title: Text('Home',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-          ),
-        ),
-        actions: <Widget>[
-          Icon(Icons.search),
-        ],
+      appBar: AppBar(
+        title: Text('Profile'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('This is mod 5 Assignment',style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: OrientationBuilder(
+        builder: (context, orientation){
+          final isLandscape = orientation == Orientation.landscape;
+          final crossAxisCount = isLandscape ? 3 : 3;
+
+          if (isLandscape) {
+            return  Row(
               children: [
-          RichText(text: TextSpan(
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.amber,
-            ),
-            children: [
-              TextSpan(
-                text: 'My ',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.deepOrange
-                )
-              ),
-              TextSpan(
-                text: 'phone',
-                style: TextStyle(
-                  fontSize: 10,
-                  color: Colors.lightBlue
+                Expanded(
+                  flex: 1,
+                  child: ClipOval(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width,
+                      child: Image.network(
+                          'https://media.gettyimages.com/id/1450131991/photo/lionel-messi-of-argentina-kisses-the-fifa-world-cup-qatar-2022-winners-trophy-after-the-fifa.jpg?s=612x612&w=0&k=20&c=wliZkz5XPFqvJqs5rqt9s2sNzsfRDxF-fHHFDyOQ-LE=',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
                 ),
-              ),
-
-              TextSpan(
-                text: ' name',
-                style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.purple
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          "Messi (G.O.A.T)",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                        child: Text(
+                            'Lionel Messi has conquered his final peak. Lionel Messi has shaken hands with paradise. The little boy from Rosario, Santa Fe, has just pitched up in heaven.'),
+                      ),
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                          SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: crossAxisCount,
+                            crossAxisSpacing: 0.8,
+                            childAspectRatio: isLandscape ? 1.1 : .8,
+                          ),
+                          itemCount: myItems.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              child: Container(
+                                margin: const EdgeInsets.all(5),
+                                width: double.infinity,
+                                height: isLandscape ? 100 : 150,
+                                child: Image.network(
+                                  myItems[index]['img']!,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              TextSpan(
-                text: ' Your phone name',
-              ),
-            ]
-          )
-          )
               ],
-            )
-          ],
-        ),
-      ),
-      );
-  }
+            );
+          } else {
+            return Column(
+              children: [
+                Expanded(
+                  child: ClipOval(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width,
+                      child: Image.network(
+                          'https://media.gettyimages.com/id/1450131991/photo/lionel-messi-of-argentina-kisses-the-fifa-world-cup-qatar-2022-winners-trophy-after-the-fifa.jpg?s=612x612&w=0&k=20&c=wliZkz5XPFqvJqs5rqt9s2sNzsfRDxF-fHHFDyOQ-LE=',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          "Messi (G.O.A.T)",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                        child: Text(
+                            'Lionel Messi has conquered his final peak. Lionel Messi has shaken hands with paradise. The little boy from Rosario, Santa Fe, has just pitched up in heaven.'),
+                      ),
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                          SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: crossAxisCount,
+                            crossAxisSpacing: 0.8,
+                            childAspectRatio: isLandscape ? 1.1 : .8,
+                          ),
+                          itemCount: myItems.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              child: Container(
+                                margin: const EdgeInsets.all(5),
+                                width: double.infinity,
+                                height: isLandscape ? 100 : 150,
+                                child: Image.network(
+                                  myItems[index]['img']!,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ) ;
+          }
+         
 
+        },
+      ),
+    );
+  }
 }
+
+
