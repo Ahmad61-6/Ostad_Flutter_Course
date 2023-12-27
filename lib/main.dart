584974b65b0bb6a483fec6d69bc937d7576fddfe
Ahmad61_6 <ahmadmizan210@gmail.com>
2023-12-27 18:58:40 +0600

    --Mod_9_liveTest

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,76 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_grid/responsive_grid.dart';
 
-void main(){
-runApp(Myapp());
-}
+void main() => runApp(const MyApp());
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return const MaterialApp(
+      title: 'Live Test 09',
+      home: ButtonColorChangeScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ButtonColorChangeScreen extends StatefulWidget {
+  const ButtonColorChangeScreen({super.key});
+
+  @override
+  _ButtonColorChangeScreenState createState() =>
+      _ButtonColorChangeScreenState();
+}
+
+class _ButtonColorChangeScreenState extends State<ButtonColorChangeScreen> {
+  MySnackBar(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  List<String> buttonSizes = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+  String? selectedSize;
+
+  void handleButtonTap(String size) {
+    setState(() {
+      selectedSize = size;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Responsive Grid"),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        title: const Text('Size Selector'),
+        centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: ResponsiveGridRow(
-          children: [
-            ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.black,
-                )
-            ),
-            ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.green,
-                )
-            ),
-            ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.red,
-                )
-            ),ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.yellow,
-                )
-            ),ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.purple,
-                )
-            ),
-            ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.blueAccent,
-                )
-            ),
-            ResponsiveGridCol(
-              xl: 3, lg: 3, md: 4, sm: 12,
-                xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.grey,
-                )
-            ),
-
-          ],
+      body: Center(
+        child: GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 4,
+          padding: const EdgeInsets.all(15.0),
+          mainAxisSpacing: 16.0,
+          crossAxisSpacing: 16.0,
+          children: buttonSizes.map((size) {
+            return ElevatedButton(
+              onPressed: () {
+                MySnackBar("You have pressed size: $size", context);
+                return handleButtonTap(size);
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                backgroundColor:
+                size == selectedSize ? Colors.amber : Colors.grey,
+              ),
+              child: Text(size),
+            );
+          }).toList(),
         ),
       ),
     );
   }
 }
-
